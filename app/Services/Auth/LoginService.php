0eb608f05200c5fe35190039574474ae831ec813
LareesDevelopers <52363124+SanjayaDev@users.noreply.github.com>
2023-05-06 06:46:18 +0700

    Create session token when login

--- app/Services/Auth/LoginService.php
+++ app/Services/Auth/LoginService.php
@@ -1,5 +1,6 @@
 <?php namespace App\Services\Auth;
 
+use App\Models\SessionToken;
 use App\Models\User;
 use App\Services\Cores\BaseService;
 use Illuminate\Http\Request;
@@ -9,8 +10,31 @@ use Illuminate\Support\Facades\Hash;
 class LoginService extends BaseService
 {
   /**
+   * Create a session token
+   *
+   * @param int $user_id
+   */
+  private function create_session_token($user_id)
+  {
+    do {
+      $token = random_string(20);
+      $check_exists = SessionToken::query()->where("session_token", $token)->exists();
+    } while ($check_exists);
+
+    SessionToken::create([
+      "user_id" => $user_id,
+      "session_token" => $token,
+      "active_time" => date("Y-m-d H:i:s"),
+      "expire_time" => date("Y-m-d H:i:s", strtotime("+15minute")),
+      "is_login" => 1
+    ]);
+
+    session(["session_token" => $token]);
+  }
+
+  /**
    * Store login
-   * 
+   *
    * @param Request $request
    */
   public function login(Request $request)
@@ -28,6 +52,7 @@ class LoginService extends BaseService
         $remember = $request->has("remember_me");
         Auth::loginUsingId($get_user->id, $remember);
 
+        $this->create_session_token($get_user->id);
         $response->status = TRUE;
         $response->status_code = 200;
         $response->message = "Success!";
